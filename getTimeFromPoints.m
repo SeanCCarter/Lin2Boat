a63ce6095f82eb3d854efefab3db4a75679822b9
Rebecca Jordan <rebecca.jordan@students.olin.edu>
2015-10-19 14:13:02 -0400

    A working bad-sailing-choices generator

--- getTimeFromPoints.m
+++ getTimeFromPoints.m
@@ -5,22 +5,36 @@ function res = getTimeFromPoints(startPoint, endPoint, Points, windX,windY)
     
     time = 0;
     for i = 1:length(X)-1
+        %determine relative wind angle
         angle = windangle(X(i,:),X(i+1,:));
-        if angle > 180
-            angle = 360 -angle;
+        if angle > pi
+            angle = 2*pi - angle;
         end
-        speed = norm(windX,windY)*(angle-pi/6)/(pi);
-        if speed > 0
+        
+        %determine speed on this tack
+        speed = speedofboat(angle);
+        
+        %determine how long travel to the next point will take
+        %if speed > 0
             time = time + distancebetweenpoints(X(i,:),X(i+1,:))/speed;     
-        else
-            time = time + distancebetweenpoints(X(i,:),X(i+1,:))*100000;
+        %else
+        %    time = time + distancebetweenpoints(X(i,:),X(i+1,:))*100000;
+        %end
+        
+        %add penalty for tacking
+        if i < (length(X) - 1)
+            time = time + tacktime(X(i,:), X(i+1,:), X(i+2,:));
         end
         
     end
     res = time;
 
-    function res = speedofboat()
-        res = 10;
+    function res = speedofboat(ang)
+        spd = norm([windX,windY])*(ang)/(pi);
+        if ang < 30
+            spd = abs(ang/10);
+        end
+        res = spd;
     end
     
     function res = distancebetweenpoints(p1,p2)
@@ -30,10 +44,39 @@ function res = getTimeFromPoints(startPoint, endPoint, Points, windX,windY)
     end
 
     function res = windangle(p1,p2)
+        %given the start and end points of a leg
+        %returns the angle between the wind and the boat's heading
         pathx = p2(1) - p1(1);
         pathy = p2(2) - p1(2);
         wind = [windX, windY];
         path = [pathx, pathy];
-        res = acos(dot(wind,path)/(norm(wind)*norm(path)));
+        windang = atan(windY/windX);
+        pathang = atan(pathy/pathx);
+        res = pathang - windang;
+        %res = acos(dot(wind,path)/(norm(wind)*norm(path)));
     end
+
+    function res = tacktime(p1, p2, p3)
+        %given three waypoints
+        %determines how long it takes to adjust course at p2
+        angle1 = windangle(p1, p2);
+        disp(angle1)
+        if angle1 > pi
+            angle1 = angle1 - 2*pi;
+        end
+        
+        angle2 = windangle(p2, p3);
+        if angle2 > pi
+            angle2 = angle2 - 2*pi;
+        end
+              
+        if sign(angle1) == sign(angle2)
+            delay = 0;
+        else
+            delay = 1;
+        end
+
+        res = delay;
+    end
+        
 end
--- letsdoathing.m
+++ letsdoathing.m
@@ -1,31 +1,39 @@
 function res = letsdoathing(nWaypoints, startPoint, endPoint)
     % set number of waypoints, start point, and end point
     nWaypoints = 4;
-    startPoint = [2,-1];
+    startPoint = [0,0];
     endPoint = [1,1];
     
     % wangle is the direction the wind is COMING FROM
     % wind speed is in knots
-    wangle = 3*pi/4;
+    wangle = 5*pi/4;
     wspeed = 2;
     % x and y values of wind vector are calculated
     windX = wspeed*cos(wangle);
     windY = wspeed*sin(wangle);
     
+    % generates a random squiggle sort of between the points as first guess
     X0 = [];
-    %Navigating from -1,-1 to 1,1
     for i = 1:nWaypoints
-        X0(i,1) = startPoint(1) + ((endPoint(1) - startPoint(1))/(nWaypoints+1)) * i;
-        X0(i,2) = startPoint(2) + ((endPoint(2) - startPoint(2))/(nWaypoints+1)) * i;
+        X0(i,1) = startPoint(1) + (rand*(endPoint(1) - startPoint(1))/(nWaypoints+1)) * i;
+        X0(i,2) = startPoint(2) + (rand*(endPoint(2) - startPoint(2))/(nWaypoints+1)) * i;
     end
     
-    X0(2,2) = 0.1
+    %X0(1,1) = -0.1
+    %X0(2,1) = .1
     
+    hold on
+        
     [x, fval] = fminunc(@(Points)getTimeFromPoints(startPoint, endPoint, Points, windX, windY), X0)
     
     clf
     thing = [startPoint;x;endPoint];
-    plot(thing(:,1),thing(:,2))
+    hold on
+    plot(X0(:,1), X0(:,2),'g')
+      quiver(1,1,windX, windY)
+    plot(thing(:,1),thing(:,2),'r')
+    plot(thing(:,1),thing(:,2),'r.')
     
+    axis equal
     res = fval;    
 end
