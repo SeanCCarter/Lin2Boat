6caec211c0be25c2d8b33f9714fd63a2be2ec490
Greenlightrj <rebecca.jordan@students.olin.edu>
2015-10-29 01:14:09 -0400

    nicely commented code

--- getTimeFromPoints.m
+++ getTimeFromPoints.m
@@ -1,24 +1,33 @@
-function res = getTimeFromPoints(startPoint, endPoint, Points, windX,windY)
-    %Points is a matrix X,Y coordinates
+function res = getTimeFromPoints(startPoint, endPoint, Points, windX, windY)
+    % Objective function for letsdoathing.m
+    % takes in [x, y] start and end points, a list of waypoints, 
+    % and matrices of the x and y components of wind at each point.
     
+    % append the start and end points to the matrix of waypoints
     X = [startPoint;Points;endPoint];
     
+    % initialize time taken
     totalTime = 0;
+    
     for step = 1:length(X)-1
+        % interpolate between each two consecutive points
         interpolated_path = makeMorePoints(X(step,:),X(step+1,:), 15);
+        
+        % add time taken to sail this segment
         totalTime = totalTime + speedOfPath(interpolated_path);
-        %add penalty for tacking
+        
+        % add penalty for tacking on all but the last step
         if step < (length(X) - 1)
             tackwindx = interp2(windX, X(step+1,1), X(step+1,2));
             tackwindy = interp2(windY, X(step+1,1), X(step+1,2));
             totalTime = totalTime + tacktime(X(step,:), X(step+1,:), X(step+2,:), tackwindx, tackwindy);
         end
     end
+    % return time taken
     res = totalTime;
     
     function res = makeMorePoints(p1, p2, n)
-        %n is the number of extra points that we want to add
-        %(for interpolation purposes)
+        % returns a list of n points between [x,y] points p1 and p2
         res =[];
         for i = 1:n+2
             res(i,1) = p1(1) + ((p2(1) - p1(1))/(n+1)) * (i-1);
@@ -26,33 +35,43 @@ function res = getTimeFromPoints(startPoint, endPoint, Points, windX,windY)
         end
     end
            
-
     function res = speedOfPath(path)
+        % takes in a list of waypoints
+        % returns the time it takes to sail that path
+        % not counting tacking delays
+        
+        % use MATLAB's built in interpolation to find wind along the path
         X_wind = interp2(windX, path(:,1), path(:,2), 'linear');
         Y_wind = interp2(windY, path(:,1), path(:,2), 'linear');
+        
+        % initialize time taken
         time = 0;
         for i = 1:length(path)-1
-            %determine relative wind angle
+            % determine relative wind angle
             angle = abs(windangle(path(i,:), path(i+1,:), X_wind(i), Y_wind(i)));
             
-            %Finding the boat speed
-            %ooh aah limacon
+            % Finding the boat speed based on relative wind angle
             speed = norm([X_wind(i), Y_wind(i)])*(0.45*cos(angle) + 0.55);
+            
+            % add time for this leg to total time
             time = time + distancebetweenpoints(path(i,:),path(i+1,:))/speed;
         end
+        
+        % return time taken
         res = time;
     end
     
     function res = distancebetweenpoints(p1,p2)
+        % simple distance between two points calculation
         dx = p2(1) - p1(1);
         dy = p2(2) - p1(2);
         res = norm([dx,dy]);
     end
 
     function res = windangle(p1,p2, wX, wY)
-        %given two [x,y] points and the wind X and Y values
-        %returns the angle between the wind and the boat heading
-        %from -pi to pi. starboard tack is positive.
+        % given two [x,y] points and the wind X and Y values
+        % returns the angle between the wind and the boat heading
+        % from -pi to pi. starboard tack is positive.
         pathx = p2(1) - p1(1);
         pathy = p2(2) - p1(2);
         wnd = atan2(wY,wX);
@@ -66,10 +85,12 @@ function res = getTimeFromPoints(startPoint, endPoint, Points, windX,windY)
     end
     
     function res = tacktime(p1, p2, p3, wx, wy)
-        %given three waypoints
-        %determines how long it takes to adjust course at p2
+        % given three waypoints and the x and y wind components,
+        % determines whether the boat changes tack at p2
         angle1 = windangle(p1, p2, wx, wy);
         angle2 = windangle(p2, p3, wx, wy);
+        % starboard tack is a positive angle, port tack is negative.
+        % sign of angle changes if tack changes
         if sign(angle1) == sign(angle2)
             delay = 0;
         else
--- letsdoathing.m
+++ letsdoathing.m
@@ -1,4 +1,10 @@
 function res = letsdoathing(nWaypoints, startPoint, endPoint)
+    % Sean Carter and Rebecca Jordan's Linearity II Optimization Project
+    % takes in a number of waypoints to place, 
+    % and [x, y] start and end points. 
+    % plots the fastest sailing path across a randomly generated windfield
+    % returns the time taken to reach endpoint
+
     % set number of waypoints, start point, and end point
     nWaypoints = nWaypoints;
     startPoint = startPoint;
@@ -6,11 +12,13 @@ function res = letsdoathing(nWaypoints, startPoint, endPoint)
     
     % wangle is the direction the wind is COMING FROM
     % wind speed is in m/s
-
     wangle = 3*pi/4 ;
     wspeed = 2;
-    % x and y values of wind vector are calculated
+    
+    % x and y matrices are generated based on the wind speed and direction
     % n is the number of values in the grid
+    % at any [x, y] point the x and y components of the wind vectors are 
+    % the values at those indices in the x and y matrices respectively.
     n = 30;
     windX = makeWindFun(n,n);
     windY = makeWindFun(n,n);
@@ -22,20 +30,28 @@ function res = letsdoathing(nWaypoints, startPoint, endPoint)
         X0(i,2) = startPoint(2) + (rand*(endPoint(2) - startPoint(2))/(nWaypoints+1)) * i;
     end
     
-    hold on
-    
+    % run the optimization
     [x, fval] = fmincon(@(Points)getTimeFromPoints(startPoint, endPoint, Points, windX, windY), X0, [], [], [], [], ones(nWaypoints, 2), Inf)
     
+    %append the start and end points to the list of waypoints
+    fullpath = [startPoint;x;endPoint];
+    
+    %graphing code
     clf
-    thing = [startPoint;x;endPoint];
     hold on
-
+    
+    % initial guess
     %plot(X0(:,1), X0(:,2),'g')
+    
+    % wind vector
     quiver(-1.*windX, -1.*windY)
-    plot(thing(:,1),thing(:,2),'r')
-    plot(thing(:,1),thing(:,2),'r.')
+    
+    % path, with dots at waypoints
+    plot(fullpath(:,1),fullpath(:,2),'r')
+    plot(fullpath(:,1),fullpath(:,2),'r.')
+    
     axis([10,20,10,20])
-    legend('First Guess', 'Wind', 'Optimal Path')
+    legend('Wind', 'Optimal Path')
     axis equal
     res = fval;    
 end
